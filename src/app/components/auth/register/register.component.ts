aca8eccb1019e19877fb7727ba247f099c696659
Mahmoud Rezq <re00zq@gmail.com>
2023-12-08 10:59:33 +0200

    adding register form validation (view)

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-    rePassword: new FormControl(),
-    phone: new FormControl(),
+    name: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(30),
+    ]),
+    rePassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(\\+201|01|00201)[0-2,5]{1}[0-9]{8}$'),
+    ]),
   });
+
+  handleRegister(form: FormGroup) {
+    console.log(form);
+  }
 }
